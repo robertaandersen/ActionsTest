473861bc372e2271d0d8f57a341e49feae967444
Róberta Andersen <roberta@andes.is>
2024-11-27 15:35:25 +0000

    unicorn pipe

--- apps/unicorn/src/app/app.tsx
+++ apps/unicorn/src/app/app.tsx
@@ -32,7 +32,7 @@ export function App() {
           element={
             <div>
               This is the generated root route.{' '}
-              <Link to="/page-2">Click asdf for page 2.</Link>
+              <Link to="/page-2">Click here for page 2.</Link>
             </div>
           }
         />
