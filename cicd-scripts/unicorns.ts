8d0af36e8adc316039ffca3e8264404ccc16b032
Róberta Andersen <roberta@andes.is>
2024-11-27 14:51:24 +0000

    unicorn pipe

--- cicd-scripts/unicorns.ts
+++ cicd-scripts/unicorns.ts
@@ -1,1 +1,33 @@
-console.log("Hallo, ich bin ein Einhorn 🦄");
+import yargs  from 'yargs';
+
+const unicornApps = [
+ 'unicorn'
+]
+
+
+export interface AffectedProjects {
+    affected: string[]
+  }
+
+yargs(process.argv.slice(2))
+.command<AffectedProjects>(
+    ['unicorn-affected'],
+    'Returns true if the affected projects contain a unicorn',
+    (yargs)=>
+      yargs
+        .option('affected', {
+          requiresArg: true,
+          alias: ['affected'],
+          type: 'array',
+          demandOption: true,
+        }),
+        unicornAffected,
+  )
+.parse();
+
+
+
+function unicornAffected(args: AffectedProjects) {
+  process.exit(args.affected.some(item => unicornApps.includes(item)) ? 0 : 1);
+}
+
