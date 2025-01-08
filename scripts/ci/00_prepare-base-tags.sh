7133b728d80f0071a7b281a1ace66fc42163b402
Róberta Andersen <roberta@andes.is>
2025-01-08 13:31:12 +0000

    chore: poke the pipe
    chore: poke the pipe
    
    chore: poke the pipe
    
    chore: poke the pipe
    
    adding main
    
    chore: poke the pipe
    
    chore: poke the pipe
    
    token
    
    chore: poke the pipe
    
    chore: poke the pipe
    
    chore: poke the pipe
    
    cleanup
    
    cleanup
    
    cleanup
    
    clean

--- scripts/ci/00_prepare-base-tags.sh
+++ scripts/ci/00_prepare-base-tags.sh
@@ -4,7 +4,10 @@ set -euxo pipefail
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ROOT="$DIR/../.."
 
+echo "ROOT IS $ROOT"
+
 tempRepo=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
+
 cp -r "$ROOT/.github/actions/dist/." "$tempRepo"
 
 LAST_GOOD_BUILD=$(DEBUG="*,-simple-git" REPO_ROOT="$ROOT" node "$tempRepo/main.js")
@@ -18,7 +21,7 @@ LAST_GOOD_BUILD_SHA=$(echo "$LAST_GOOD_BUILD" | jq -r '.sha')
 LAST_GOOD_BUILD_BRANCH=$(echo "$LAST_GOOD_BUILD" | jq -r '.branch')
 LAST_GOOD_BUILD_RUN_NUMBER=$(echo "$LAST_GOOD_BUILD" | jq -r '.run_number')
 BUILD_REF=$(echo "$LAST_GOOD_BUILD" | jq -r '.ref')
->&2 echo "Last successful build is with SHA '$LAST_GOOD_BUILD_SHA', branch '$LAST_GOOD_BUILD_BRANCH' and number '$LAST_GOOD_BUILD_RUN_NUMBER'"
+echo >&2 "Last successful build is with SHA '$LAST_GOOD_BUILD_SHA', branch '$LAST_GOOD_BUILD_BRANCH' and number '$LAST_GOOD_BUILD_RUN_NUMBER'"
 if [[ "$BUILD_REF" != "$LAST_GOOD_BUILD_SHA" ]]; then
   echo "This will be an incremental build from a previous successful run in this PR. See parents of the commit below."
   git log -1 "$BUILD_REF"
@@ -32,4 +35,4 @@ else
   BASE="$BUILD_REF"
 fi
 export BASE
->&2 echo "Last successful docker tag '$LAST_GOOD_BUILD_DOCKER_TAG'"
+echo >&2 "Last successful docker tag '$LAST_GOOD_BUILD_DOCKER_TAG'"
