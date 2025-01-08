4a847a1ed24a7a2ad9ff14ea164fdcefbebe1213
Róberta Andersen <roberta@andes.is>
2025-01-08 15:10:04 +0000

    chore: poke the pipe

--- scripts/ci/00_prepare-base-tags.sh
+++ scripts/ci/00_prepare-base-tags.sh
@@ -9,6 +9,10 @@ echo "ROOT IS $ROOT"
 tempRepo=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 
 cp -r "$ROOT/.github/actions/dist/." "$tempRepo"
+// DEBUG
+export NX_AFFECTED_ALL=true
+echo "NX_AFFECTED_ALL=$NX_AFFECTED_ALL" >>"$GITHUB_ENV"
+exit 0
 
 LAST_GOOD_BUILD=$(DEBUG="*,-simple-git" REPO_ROOT="$ROOT" node "$tempRepo/main.js")
 if echo "$LAST_GOOD_BUILD" | grep -q 'full_rebuild_needed'; then
